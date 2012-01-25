814fa504c0e055b06a75be7479752f2ed7d704b9
Roger <sent2roger@gmail.com>
2012-01-26 05:03:40 +0600

    First commit

---
+++ src/com/chess/dashboard_test/DashBoardTestActivity.java
@@ -0,0 +1,13 @@
+package com.chess.dashboard_test;
+
+import actionbarcompat.ActionBarActivity;
+import android.os.Bundle;
+
+public class DashBoardTestActivity extends ActionBarActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
