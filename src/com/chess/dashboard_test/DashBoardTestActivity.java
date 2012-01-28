01cffae3d638b7b22a1ee202196e0c959b723822
Roger <sent2roger@gmail.com>
2012-01-28 07:02:50 +0600

    dashboard added

--- src/com/chess/dashboard_test/DashBoardTestActivity.java
+++ src/com/chess/dashboard_test/DashBoardTestActivity.java
@@ -2,12 +2,57 @@ package com.chess.dashboard_test;
 
 import actionbarcompat.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class DashBoardTestActivity extends ActionBarActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class DashBoardTestActivity extends ActionBarActivity implements OnClickListener {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		View mainView = findViewById(R.id.mainView);
+		mainView.setBackgroundDrawable(new BackgroundChessDrawable(this));
+		int padding = getResources().getDrawable(R.drawable.chess_cells).getIntrinsicWidth() / 2;
+		mainView.setPadding(padding, padding, padding, padding);
+
+		findViewById(R.id.playLive).setTouchDelegate(findViewById(R.id.playLiveFrame).getTouchDelegate());
+		findViewById(R.id.playLiveFrame).setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Toast.makeText(this, "Tapped home", Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.menu_settings:
+			Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.playLiveFrame) {
+			Toast.makeText(this, "playLiveFrame", Toast.LENGTH_SHORT).show();
+		}
+		// TODO Auto-generated method stub
+
+	}
 }
